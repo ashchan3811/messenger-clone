87f4b183efb7747ded7f52745456fcfefab730b8
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2023-12-08 18:24:20 +0530

    Added loader

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -5,6 +5,7 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { User } from "@prisma/client";
 import Avatar from "@/components/Avatar";
+import LoadingModal from "@/components/LoadingModal";
 
 interface UserBoxProps {
   user: User;
@@ -30,19 +31,22 @@ const UserBox = ({ user }: UserBoxProps) => {
   }, [router, user.id]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-    >
-      <Avatar user={user} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">{user.name}</p>
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+      >
+        <Avatar user={user} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">{user.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
